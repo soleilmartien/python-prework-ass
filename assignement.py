4c8405f5a544975fd7716c0320b01e7fea971a76
soleilmartien <soleilmartien@gmail.com>
2023-01-09 23:51:48 -0500

    Add files via upload

--- assignement.py
+++ assignement.py
@@ -1,27 +1,22 @@
-def hello_name(name):
-    
-    print("hello_"+name+"!")
-hello_name('PIPI')
+def hello_name(user_name):
+    print("hello_"+user_name+"!")
 
 def first_odds():
     for x in range(50):
         print(2*x+1)
-first_odds()
 
-def max_num_in_list(list):
-    big = int(list[0])
+def max_num_in_list(a_list):
+    big = a_list[0]
     for x in list:
-        if int(x) > big:
-            big = int(x)
-    print(big)
-max_num_in_list([1,2,7,3,7,2,6,3,7,8,4,8,8,3,23,423,65,73])
+        if x > big:
+            big = x
+    return big
 
-def is_leap(year):
-    if year%4 == 0 and (year%100 != 0 or (year%100 == 0 and year%400 == 0)):
+def is_leap_year(a_year):
+    if a_year%4 == 0 and (a_year%100 != 0 or (a_year%100 == 0 and a_year%400 == 0)):
         leap = True
     else:
         leap = False
-    print(leap)
     return leap
 
 def is_consecutive(a_list):
@@ -30,11 +25,11 @@ def is_consecutive(a_list):
         if a_list[x + 1] == prem + 1:
             prem +=1
         else:
-            print(False)
-            break
+            return False
     if prem == a_list[-1]:
-        print(True)
         return True
-is_consecutive([3,4,5,6,7,8,9])
+
+
+
 
 
